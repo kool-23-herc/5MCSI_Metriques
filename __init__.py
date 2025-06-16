76efd4045b8ef0e2930d11c1f249ff7e031dab18
kool-23-herc <nilszinsou@gmail.com>
2025-06-16 12:08:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,10 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def mongraphiquehistogramme():
+    return render_template("histogramme.html")
   
   
 if __name__ == "__main__":
